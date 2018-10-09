3bcd0ade832fcc3ae87e991a4fc571b2040c5d2d
CJ Faraon <faraonc@outlook.com>
2018-10-09 15:18:06 -0700

    Fix path for proto package definition

--- generate_int_proto.sh
+++ generate_int_proto.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # generate protoc for our service
-echo "Generating internal go-proto..."
+echo "Generating internal proto..."
 
 echo "Generating grpc-sample.pb.go..."
-protoc --proto_path=int/proto --go_out=plugins=grpc:int/hwsc-grpc-sample/go-proto int/proto/grpc-sample.proto
+protoc int/hwsc-grpc-sample/proto/grpc-sample.proto --go_out=plugins=grpc:.
+
+# path example
+#protoc --proto_path=int/proto --go_out=plugins=grpc:int/hwsc-grpc-sample/go-proto int/proto/grpc-sample.proto
 
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
 const demo = require('./int/demo/demo.js');
 demo.printMsg();
 
-/* --------- hwsc-grpc-sample --------- */
-const PROTO_PATH = __dirname + '/int/hwsc-grpc-sample/go-proto/grpc-sample.go-proto';
-const protoLoader = require('@grpc/proto-loader');
-//TODO not sure how to handle additional go-proto
-const packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
+/* --------- hwsc-grpc-sample service definition --------- */
+const HWSC_GRPC_SAMPLE_PROTO_PATH = __dirname + '/int/hwsc-grpc-sample/proto/grpc-sample.proto';
+//TODO not sure how to handle additional proto
+const hwscGrpcSampleProtoPackageDefinition = protoLoader.loadSync(
+    HWSC_GRPC_SAMPLE_PROTO_PATH,
     {keepCase: true,
         longs: String,
         enums: String,
         defaults: true,
         oneofs: true
     });
-const sample_proto = grpc.loadPackageDefinition(packageDefinition).sample; // has to match grpc-sample.go-proto line 3 package
-/* --------- hwsc-grpc-sample --------- */
+const sample_proto = grpc.loadPackageDefinition(hwscGrpcSampleProtoPackageDefinition).sample; // has to match grpc-sample.proto line 3 package
+/* --------- end of hwsc-grpc-sample definition --------- */
 
 function main() {
     let client = new sample_proto.SampleService('localhost:50051',
