c0fdc829eb57ca58573c80bc04678c46d61dc4af
faraonc <faraonc@outlook.com>
2018-10-09 19:33:21 -0700

    Initial metada file svc contract.

--- generate_int_proto.sh
+++ generate_int_proto.sh
@@ -5,6 +5,8 @@ echo "Generating internal proto..."
 
 echo "Generating grpc-sample.pb.go..."
 protoc int/hwsc-grpc-sample/proto/grpc-sample.proto --go_out=plugins=grpc:.
+echo "Generating hwsc-metadata-file-svc.pb.go..."
+protoc int/hwsc-metadata-file-svc/proto/hwsc-metadata-file-svc.proto --go_out=plugins=grpc:.
 
 # path example
 #protoc --proto_path=int/proto --go_out=plugins=grpc:int/hwsc-grpc-sample/go-proto int/proto/grpc-sample.proto
