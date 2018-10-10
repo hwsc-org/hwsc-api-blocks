d488a9fd4ba7858370b9a0eb0f9e1481e9b47e48
CJ Faraon <faraonc@outlook.com>
2018-10-09 21:53:29 -0700

    Modify sample svc name to hwsc-grpc-sample-svc

--- generate_int_proto.sh
+++ generate_int_proto.sh
@@ -3,11 +3,11 @@
 # generate protoc for our service
 echo "Generating internal proto..."
 
-echo "Generating grpc-sample.pb.go..."
-protoc int/hwsc-grpc-sample/proto/grpc-sample.proto --go_out=plugins=grpc:.
+echo "Generating grpc-sample-svc.pb.go..."
+protoc int/hwsc-grpc-sample-svc/proto/grpc-sample-svc.proto --go_out=plugins=grpc:.
 echo "Generating hwsc-metadata-file-svc.pb.go..."
 protoc int/hwsc-metadata-file-svc/proto/hwsc-metadata-file-svc.proto --go_out=plugins=grpc:.
 
 # path example
-#protoc --proto_path=int/proto --go_out=plugins=grpc:int/hwsc-grpc-sample/go-proto int/proto/grpc-sample.proto
+#protoc --proto_path=int/proto --go_out=plugins=grpc:int/hwsc-grpc-sample-svc/go-proto int/proto/grpc-sample-svc.proto
 
--- index.js
+++ index.js
@@ -3,55 +3,55 @@ const protoLoader = require('@grpc/proto-loader');
 const demo = require('./int/demo/demo.js');
 demo.printMsg();
 
-// /* --------- hwsc-grpc-sample service definition --------- */
-// const HWSC_GRPC_SAMPLE_PROTO_PATH = __dirname + '/int/hwsc-grpc-sample/proto/grpc-sample.proto';
-// //TODO not sure how to handle additional proto
-// const hwscGrpcSampleProtoPackageDefinition = protoLoader.loadSync(
-//     HWSC_GRPC_SAMPLE_PROTO_PATH,
-//     {keepCase: true,
-//         longs: String,
-//         enums: String,
-//         defaults: true,
-//         oneofs: true
-//     });
-// const sample_proto = grpc.loadPackageDefinition(hwscGrpcSampleProtoPackageDefinition).sample; // has to match grpc-sample.proto line 3 package
-// /* --------- end of hwsc-grpc-sample definition --------- */
-//
-// function main() {
-//     let client = new sample_proto.SampleService('localhost:50051',
-//         grpc.credentials.createInsecure());
-//     let user;
-//     if (process.argv.length >= 3) {
-//         user = process.argv[2];
-//     } else {
-//         user = 'world';
-//     }
-//     client.sayHello({name: user}, function(err, response) {
-//         console.log('Greeting:', response.message);
-//     });
-// }
-
-/* --------- hwsc-metadata-file-svc service definition --------- */
-const HWSC_METADATA_FILE_SVC_PROTO_PATH = __dirname + '/int/hwsc-metadata-file-svc/proto/hwsc-metadata-file-svc.proto';
+/* --------- hwsc-grpc-sample-svc service definition --------- */
+const HWSC_GRPC_SAMPLE_PROTO_PATH = __dirname + '/int/hwsc-grpc-sample-svc/proto/grpc-sample-svc.proto';
 //TODO not sure how to handle additional proto
 const hwscGrpcSampleProtoPackageDefinition = protoLoader.loadSync(
-    HWSC_METADATA_FILE_SVC_PROTO_PATH,
+    HWSC_GRPC_SAMPLE_PROTO_PATH,
     {keepCase: true,
         longs: String,
         enums: String,
         defaults: true,
         oneofs: true
     });
-const sample_proto = grpc.loadPackageDefinition(hwscGrpcSampleProtoPackageDefinition).hwscMetadataFileSvc; // has to match grpc-sample.proto line 3 package
-/* --------- end of hwsc-metadata-file-svc definition --------- */
+const sample_proto = grpc.loadPackageDefinition(hwscGrpcSampleProtoPackageDefinition).sample; // has to match grpc-sample-svc.proto line 3 package
+/* --------- end of hwsc-grpc-sample-svc definition --------- */
 
 function main() {
-    let client = new sample_proto.MetadataFileService('localhost:50051',
+    let client = new sample_proto.SampleService('localhost:50051',
         grpc.credentials.createInsecure());
-
-    client.CreateMetadata({first: "Lisa", last: "Kim", url: "url"}, function(err, response) {
-        console.log('Response:', response.message);
+    let user;
+    if (process.argv.length >= 3) {
+        user = process.argv[2];
+    } else {
+        user = 'world';
+    }
+    client.sayHello({name: user}, function(err, response) {
+        console.log('Greeting:', response.message);
     });
 }
+
+// /* --------- hwsc-metadata-file-svc service definition --------- */
+// const HWSC_METADATA_FILE_SVC_PROTO_PATH = __dirname + '/int/hwsc-metadata-file-svc/proto/hwsc-metadata-file-svc.proto';
+// //TODO not sure how to handle additional proto
+// const hwscGrpcSampleProtoPackageDefinition = protoLoader.loadSync(
+//     HWSC_METADATA_FILE_SVC_PROTO_PATH,
+//     {keepCase: true,
+//         longs: String,
+//         enums: String,
+//         defaults: true,
+//         oneofs: true
+//     });
+// const sample_proto = grpc.loadPackageDefinition(hwscGrpcSampleProtoPackageDefinition).hwscMetadataFileSvc; // has to match grpc-sample-svc.proto line 3 package
+// /* --------- end of hwsc-metadata-file-svc definition --------- */
+//
+// function main() {
+//     let client = new sample_proto.MetadataFileService('localhost:50051',
+//         grpc.credentials.createInsecure());
+//
+//     client.CreateMetadata({first: "Lisa", last: "Kim", url: "url"}, function(err, response) {
+//         console.log('Response:', response.message);
+//     });
+// }
 
 main();
