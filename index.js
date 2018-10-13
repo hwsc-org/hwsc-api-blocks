d3e3ffdb1f53f42946c612de3c326f399594618c
faraonc <faraonc@outlook.com>
2018-10-13 13:53:01 -0700

    hwsc-file-svc with updated client

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-const demo = require("./int/demo/demo.js");
-const hwscGrpcSampleSvc = require("./int/hwsc-grpc-sample-svc/hwsc-grpc-sample-svc.js");
-const hwscMetadataFileSvc = require("./int/hwsc-metadata-file-svc/hwsc-metadata-file-svc.js");
-const fs = require("./int/hwsc-file-svc/hwsc-file-svc.js");
+module.exports = {
+    demo: require("./int/demo/demo.js"),
+    hwscGrpcSampleSvc: require("./int/hwsc-grpc-sample-svc/hwsc-grpc-sample-svc.js"),
+    hwscMetadataFileSvc: require("./int/hwsc-metadata-file-svc/hwsc-metadata-file-svc.js"),
+    hwscFileSvc: require("./int/hwsc-file-svc/hwsc-file-svc.js")
+};
 
 
-function callback(err, response) {
-    if (err) {
-        console.log("Error: ", err);
-    } else {
-        console.log("Response ", response)
-    }
-}
-
-
-function main() {
-    demo.printMsg();
-    // Uncomment/Comment gRPC calls below as necessary
-    // hwscGrpcSampleSvc.sayHello(callback);
-    // hwscMetadataFileSvc.createMetadataFile(callback);
-    fs.getStatus(callback);
-}
-
-main();
 
 
 
