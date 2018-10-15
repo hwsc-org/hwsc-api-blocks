238601e78d6446c1a4470fdc58e4753c6a01658f
CJ Faraon <faraonc@outlook.com>
2018-10-15 16:12:43 -0700

    Implemented addition of bson tags to pb.go

--- test_client.js
+++ test_client.js
@@ -3,32 +3,58 @@ const moment = require("moment");
 
 function callback(err, response) {
     if (err) {
-        console.log("Error: ", err);
+        console.log(err);
     } else {
         console.log("Response ", response)
     }
 }
 
-const data = {
-        lastName: "Kim",
-        firstName: "Lisa",
-        callTypeName: "some call type name",
-        groundType: "some ground type",
-        region: "some region",
-        ocean: "some ocean",
-        sensorType: "some sensor type",
-        sensorName: "some sensor name",
-        sampleRate: 1000,
-        latitude: 100.123,
-        longitude: -100.123,
-        imageUrl: [],
-        audioUrl: [],
-        videoUrl: [],
-        fileUrl: [],
-        recordTimestamp: moment().utc().unix(),
-        createTimestamp: moment().utc().unix(),
-        updateTimestamp: 0
-    };
+// Valid
+// const data = {
+//         lastName: "Kim",
+//         firstName: "Lisa",
+//         callTypeName: "some call type name",
+//         groundType: "some ground type",
+//         region: "some region",
+//         ocean: "some ocean",
+//         sensorType: "some sensor type",
+//         sensorName: "some sensor name",
+//         sampleRate: 1000,
+//         latitude: 100.123,
+//         longitude: -100.123,
+//         imageUrl: ["google.com", "bing.com"],
+//         audioUrl: ["google.com", "bing.com"],
+//         videoUrl: ["google.com", "bing.com"],
+//         fileUrl: ["google.com", "bing.com", "yahoo.com"],
+//         recordTimestamp: moment().utc().unix(),
+//         createTimestamp: 0,
+//         updateTimestamp: 0
+//     };
+
+// Nil
+const data = null;
+
+// Invalid lastName
+// const data = {
+//     lastName: "",
+//     firstName: "Lisa",
+//     callTypeName: "some call type name",
+//     groundType: "some ground type",
+//     region: "some region",
+//     ocean: "some ocean",
+//     sensorType: "some sensor type",
+//     sensorName: "some sensor name",
+//     sampleRate: 1000,
+//     latitude: 100.123,
+//     longitude: -100.123,
+//     imageUrl: ["google.com", "bing.com"],
+//     audioUrl: ["google.com", "bing.com"],
+//     videoUrl: ["google.com", "bing.com"],
+//     fileUrl: ["google.com", "bing.com", "yahoo.com"],
+//     recordTimestamp: moment().utc().unix(),
+//     createTimestamp: 0,
+//     updateTimestamp: 0
+// };
 
 function main() {
     index.demo.printMsg();
