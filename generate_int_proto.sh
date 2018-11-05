035a508b5a9f165c307549cd2e94d3f645b94180
Lisa Kim <lisakeem@outlook.com>
2018-11-04 23:23:31 -0800

    added bson tag to user.proto, modified script, compiled pb

--- generate_int_proto.sh
+++ generate_int_proto.sh
@@ -3,10 +3,31 @@
 # generate protoc for our service
 echo "Generating internal proto..."
 
+
+
+# SAMPLE SERVICE
 echo "Generating hwsc-grpc-sample-svc.pb.go..."
 protoc int/hwsc-grpc-sample-svc/proto/hwsc-grpc-sample-svc.proto \
     --go_out=plugins=grpc:.
 
+
+
+# USER SERVICE
+echo "Generating hwsc-user-svc.pb.go, user.pb.go..."
+OUTPUT_ROOT="./int/hwsc-user-svc/proto/"
+protoc \
+    -I ./lib/ \
+    -I ${OUTPUT_ROOT} \
+    --go_out=plugins=grpc:${OUTPUT_ROOT} \
+    user.proto hwsc-user-svc.proto
+
+protoc-go-inject-tag -input=${OUTPUT_ROOT}hwsc-user-svc.pb.go
+protoc-go-inject-tag -input=${OUTPUT_ROOT}user.pb.go
+sed -i'.orig' -e 's/`json:"-"`/`json:"-" bson:"-"`/g' ${OUTPUT_ROOT}*.pb.go
+
+
+
+# DOCUMENT SERVICE
 echo "Generating hwsc-document-svc.pb.go, document.pb.go..."
 OUTPUT_ROOT="./int/hwsc-document-svc/proto/"
 protoc \
@@ -20,6 +41,8 @@ protoc-go-inject-tag -input=./int/hwsc-document-svc/proto/document.pb.go
 sed -i'.orig' -e 's/`json:"-"`/`json:"-" bson:"-"`/g' int/hwsc-document-svc/proto/*.pb.go
 
 
+
+# FILE TRANSACTION SERVICE
 echo "Generating hwsc-file-transaction-svc.pb2.py, hwsc-file-transaction-svc.pb2_grpc.py..."
 python3 -m grpc_tools.protoc \
     -I./int/hwsc-file-transaction-svc/proto \
@@ -28,6 +51,8 @@ python3 -m grpc_tools.protoc \
     ./int/hwsc-file-transaction-svc/proto/hwsc-file-transaction-svc.proto
 
 
+
+# APP GATEWAY SERVICE
 echo "Generating hwsc-app-gateway .ts, .js, .pb"
 OUTPUT_ROOT="./int/hwsc-app-gateway-svc/proto/"
 protoc \
