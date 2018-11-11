06605ec767a7da6a4560323793bb1cbbddec6809
CJ Faraon <faraonc@outlook.com>
2018-11-10 17:04:52 -0800

    Learn from this example

--- int/hwsc-file-transaction-svc/hwsc-file-transaction-svc.js
+++ int/hwsc-file-transaction-svc/hwsc-file-transaction-svc.js
@@ -72,37 +72,38 @@ function uploadFile(filePath, fileName, callback) {
   });
 
   // client send the upload file name to server
-  server.send({ fileName });
+  server.write({ fileName: 'some awesome filename' });
+  server.end();
 
-  // open the file, and read/pipe the first 1024 bytes of the file
-  const readStream = fs.createReadStream(fileLocation, { hightWaterMark: 1024 });
-
-  // const readable = getReadableStreamSomehow();
-  readStream.on('readable', () => {
-    let chunk;
-    // set buffer = 1st 1024 bytes
-    readStream.pipe(server);
-    while ((chunk = readStream.read()) !== null) {
-      // send the buffer
-      server.send({ buffer: chunk });
-      // Read/pipe and send the next 1024 bytes
-      readStream.pipe(server);
-    }
-  });
-
-  readStream.on('end', () => {
-    server.end();
-  });
-
-  readStream.on('error', (err) => {
-    console.error('Error!');
-    server.end(err);
-  });
-
-  readStream.on('cancel', (err) => {
-    console.error('Cancel!');
-    server.end(err);
-  });
+  // // open the file, and read/pipe the first 1024 bytes of the file
+  // const readStream = fs.createReadStream(fileLocation, { hightWaterMark: 1024 });
+  //
+  // // const readable = getReadableStreamSomehow();
+  // readStream.on('readable', () => {
+  //   let chunk;
+  //   // set buffer = 1st 1024 bytes
+  //   readStream.pipe(server);
+  //   while ((chunk = readStream.read()) !== null) {
+  //     // send the buffer
+  //     server.send({ buffer: chunk });
+  //     // Read/pipe and send the next 1024 bytes
+  //     readStream.pipe(server);
+  //   }
+  // });
+  //
+  // readStream.on('end', () => {
+  //   server.end();
+  // });
+  //
+  // readStream.on('error', (err) => {
+  //   console.error('Error!');
+  //   server.end(err);
+  // });
+  //
+  // readStream.on('cancel', (err) => {
+  //   console.error('Cancel!');
+  //   server.end(err);
+  // });
 }
 
 module.exports = {
