08f77f5e3aa297f079836888827c961e7d602d69
CJ Faraon <faraonc@outlook.com>
2019-03-02 12:45:18 -0800

    Update to newest protoc proto-gen-go grpc

--- int/hwsc-user-svc/user/hwsc-user-svc.pb.go
+++ int/hwsc-user-svc/user/hwsc-user-svc.pb.go
@@ -4,10 +4,10 @@
 package user
 
 import (
+	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
 	lib "github.com/hwsc-org/hwsc-api-blocks/lib"
-	context "golang.org/x/net/context"
 	grpc "google.golang.org/grpc"
 	math "math"
 )
@@ -21,7 +21,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type UserRequest struct {
 	Identification       *lib.Identification `protobuf:"bytes,1,opt,name=identification,proto3" json:"identification,omitempty"`
@@ -176,54 +176,11 @@ func (m *UserResponse) GetUserCollection() []*lib.User {
 	return nil
 }
 
-// XXX_OneofFuncs is for the internal use of the proto package.
-func (*UserResponse) XXX_OneofFuncs() (func(msg proto.Message, b *proto.Buffer) error, func(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error), func(msg proto.Message) (n int), []interface{}) {
-	return _UserResponse_OneofMarshaler, _UserResponse_OneofUnmarshaler, _UserResponse_OneofSizer, []interface{}{
+// XXX_OneofWrappers is for the internal use of the proto package.
+func (*UserResponse) XXX_OneofWrappers() []interface{} {
+	return []interface{}{
 		(*UserResponse_Code)(nil),
 	}
-}
-
-func _UserResponse_OneofMarshaler(msg proto.Message, b *proto.Buffer) error {
-	m := msg.(*UserResponse)
-	// status
-	switch x := m.Status.(type) {
-	case *UserResponse_Code:
-		b.EncodeVarint(1<<3 | proto.WireVarint)
-		b.EncodeVarint(uint64(x.Code))
-	case nil:
-	default:
-		return fmt.Errorf("UserResponse.Status has unexpected type %T", x)
-	}
-	return nil
-}
-
-func _UserResponse_OneofUnmarshaler(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error) {
-	m := msg.(*UserResponse)
-	switch tag {
-	case 1: // status.code
-		if wire != proto.WireVarint {
-			return true, proto.ErrInternalBadWireType
-		}
-		x, err := b.DecodeVarint()
-		m.Status = &UserResponse_Code{uint32(x)}
-		return true, err
-	default:
-		return false, nil
-	}
-}
-
-func _UserResponse_OneofSizer(msg proto.Message) (n int) {
-	m := msg.(*UserResponse)
-	// status
-	switch x := m.Status.(type) {
-	case *UserResponse_Code:
-		n += 1 // tag and wire
-		n += proto.SizeVarint(uint64(x.Code))
-	case nil:
-	default:
-		panic(fmt.Sprintf("proto: unexpected type %T in oneof", x))
-	}
-	return n
 }
 
 func init() {
