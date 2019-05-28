4ab316561d30237d6c5ca66475f9873e88f9b874
Alex Tang <act.alextang@gmail.com>
2019-05-27 23:11:42 -0700

    Add integration test for file-transaction-svc

--- integration_tests/test_file_transaction_svc_client.js
+++ integration_tests/test_file_transaction_svc_client.js
@@ -9,12 +9,30 @@ function callback(err, response) {
 }
 const filePath = './res';
 const fileName = 'cat.jpg';
-const uuid = '1234abcd5454efef8842ll3faq';
+
+const uuids = [
+  '1234abcd5454efef8842ll3faq',
+  '1234abcd5454efef8842ll3faq',
+  '1234abcd5454efef8842ll3fa',
+];
 
 function main() {
-  index.hwscFileTransactionSvc.getStatus(callback);
-  // index.hwscFileTransactionSvc.uploadFile(filePath, fileName, uuid, callback);
-  // index.hwscFileTransactionSvc.createUserFolder(uuid, callback);
+  switch (process.argv[2]) {
+    case '0':
+      index.hwscFileTransactionSvc
+        .getStatus(callback);
+      break;
+    case '1':
+      index.hwscFileTransactionSvc
+        .uploadFile(process.argv[3], process.argv[4], process.argv[5], callback);
+      break;
+    case '2':
+      index.hwscFileTransactionSvc
+        .createUserFolder(uuids[parseInt(process.argv[3])], callback);
+      break;
+    default:
+      console.error('invalid arg');
+  }
 }
 
 main();
